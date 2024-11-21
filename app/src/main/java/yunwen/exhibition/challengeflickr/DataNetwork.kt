5e26fb53247dd72c2db1ad78512d7815ff3fc17b
Alvin(Yunwen) Lei <yl1874@nyu.edu>
2024-11-20 20:23:06 -0600

    merge

--- app/src/main/java/yunwen/exhibition/challengeflickr/DataNetwork.kt
+++ app/src/main/java/yunwen/exhibition/challengeflickr/DataNetwork.kt
@@ -1,46 +1,23 @@
 package yunwen.exhibition.challengeflickr
 
-import com.google.gson.annotations.SerializedName
-
-
 data class DataNetwork(
-    @SerializedName("title")
     val title: String,
-    @SerializedName("link")
     val link: String,
-    @SerializedName("description")
     val description: String,
-    @SerializedName("modified")
     val modified: String,
-    @SerializedName("generator")
     val generator: String,
-    @SerializedName("items")
     val items: List<ItemNetwork>
 )
 
 data class ItemNetwork(
-    @SerializedName("title")
     val title: String,
-    @SerializedName("link")
-    val link: String,
-    @SerializedName("media")
     val media: Media,
-    @SerializedName("date_taken")
-    val dateTaken: String,
-    @SerializedName("description")
     val description: String,
-    @SerializedName("published")
     val published: String,
-    @SerializedName("author")
     val author: String,
-    @SerializedName("author_id")
-    val authorId: String,
-    @SerializedName("tags")
-    val tags: String
 )
 
 data class Media(
-    @SerializedName("m")
     val m: String,
 )
 
